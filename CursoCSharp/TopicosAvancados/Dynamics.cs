bad599e45bc733fe4ea51fdf8428d848dd256f24
leoespindula <espindulasystem.dev@gmail.com>
2022-08-17 11:54:21 -0300

    versionFull

--- CursoCSharp/TopicosAvancados/Dynamics.cs
+++ CursoCSharp/TopicosAvancados/Dynamics.cs
@@ -10,7 +10,18 @@ namespace CursoCSharp.TopicosAvancados
     {
         public static void Executar()
         {
+            dynamic meuObjeto = "teste";
+            meuObjeto = 3;
 
+            meuObjeto++;
+            Console.WriteLine(meuObjeto);
+
+            dynamic aluno = new System.Dynamic.ExpandoObject();
+            aluno.Nome = "Maria";
+            aluno.Nota = 9.9;
+            aluno.Idade = 25;
+
+            Console.WriteLine($"{aluno.Nome} {aluno.Idade} {aluno.Nota}");
         }
     }
 }
