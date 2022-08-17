1a1b2ef55d4a93c0b599a89540ef24d0d813a9d3
leoespindula <espindulasystem.dev@gmail.com>
2022-08-17 11:12:16 -0300

    version10.13

---
+++ CursoCSharp/TopicosAvancados/Dynamics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    internal class Dynamics
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
